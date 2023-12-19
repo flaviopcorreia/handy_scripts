21e095f9ab963ca911382054539f9badd6a8cf57
flaviopcorreia <111467171+flaviopcorreia@users.noreply.github.com>
2023-12-19 15:00:46 -0300

    Update azad_move_users_to_groups.ps1

--- scripts/az_entra_add_users_to_groups/azad_move_users_to_groups.ps1
+++ scripts/az_entra_add_users_to_groups/azad_move_users_to_groups.ps1
@@ -1,4 +1,3 @@
-#Informar o ID do grupo que deseja adicionar os usuários 'ID_AQUI'
 $targetgroup = 'ID_AQUI'
 
 #Listar quantidade de usuários no grupo
@@ -7,10 +6,8 @@ Get-AzADGroupMember -GroupObjectId $targetgroup | Measure-Object
 #Fazer backup dos membros do grupo em um arquivo CSV.
 Get-AzADGroupMember -GroupObjectId $targetgroup | export-csv 'path:\backup.csv' 
 
-#Adicionar os usuários ao grupo. No item "path:\file.txt" abaixo, adicionar o path do arquivo txt que contenha o ID dos usuários que queira adicionar aos grupo.
+#Adicionar os usuários ao grupo. 
 $files = Get-Content "path:\file.txt"
-
-#Função para adicionar os usuários ao respectivo grupo
 foreach ($file in $files) {
     $move = Add-AzADGroupMember -TargetGroupObjectId $targetgroup -MemberObjectId $file 
 }
